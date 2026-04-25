eaeeb94d7714d1338b9b783d48053c342dd3f149
harshulgurjar <harshulgurjar7@gmail.com>
2026-04-25 22:08:28 +0530

    Add files via upload

---
+++ day 9/combinationsum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void findcombinations(int idx,vector<int>&arr,int target,vector<vector<int>>&ans,vector<int>&ds){
+        if(idx == arr.size()){
+            if(target == 0){
+                ans.push_back(ds);
+            }
+            return;
+        }
+
+        if(arr[idx] <= target){
+            ds.push_back(arr[idx]);
+            findcombinations(idx,arr,target-arr[idx],ans,ds);
+            ds.pop_back();
+        }
+        findcombinations(idx+1,arr,target,ans,ds);
+    }
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>> ans;
+        vector<int> ds;
+
+        findcombinations(0,candidates,target,ans,ds);
+        return ans;
+    }
+};
