e128d68d94d4c7c6e0c7ce3e603bd4c65fb5049c
harshulgurjar <harshulgurjar7@gmail.com>
2026-04-27 21:03:23 +0530

    Add files via upload

---
+++ day 11/groupanagram.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        unordered_map<string,vector<string>>mp;
+        for(auto s:strs){
+            string t=s;
+            sort(t.begin(),t.end());
+            mp[t].push_back(s);
+        }
+        vector<vector<string>>result;
+        for(auto &pair:mp){
+            result.push_back(pair.second);
+
+        }
+        return result;
+    }
+};
