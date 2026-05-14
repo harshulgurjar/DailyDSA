f215644176f17e92fe222f9aeb71e83be9e83036
harshulgurjar <harshulgurjar7@gmail.com>
2026-05-14 11:38:49 +0530

    Implement function to reverse only letters in string

---
+++ Day 27/919.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseOnlyLetters(string s) {
+         int n = s.size();
+        int l = 0, r = n -1 ;
+        while (l < r){
+            while (l < n && !isalpha(s[l])) l++ ;
+            while (r > 0 && !isalpha(s[r])) r-- ;
+            if (l < r){ // ! Critical safety check
+                swap(s[l], s[r]);
+                l++ ; r-- ;
+            }
+        }
+        return s;
+    }
+};
