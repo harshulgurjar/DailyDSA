8b9d9b092211338920749b41c434059c013d9a1f
harshulgurjar <harshulgurjar7@gmail.com>
2026-06-22 11:18:47 +0530

    Implement solution for minimum days to make bouquets

---
+++ day 47/1482.cpp
@@ -0,0 +1,47 @@
+class Solution {
+public:
+    bool canMake(vector<int>& bloomDay, int m, int k, int day) {
+        int bouquets = 0;
+        int flowers = 0;
+
+        for (int x : bloomDay) {
+            if (x <= day) {
+                flowers++;
+
+                if (flowers == k) {
+                    bouquets++;
+                    flowers = 0;
+                }
+            } else {
+                flowers = 0;
+            }
+        }
+
+        return bouquets >= m;
+    }
+
+    int minDays(vector<int>& bloomDay, int m, int k) {
+        long long need = 1LL * m * k;
+
+        if (need > bloomDay.size())
+            return -1;
+
+        int low = *min_element(bloomDay.begin(), bloomDay.end());
+        int high = *max_element(bloomDay.begin(), bloomDay.end());
+
+        int ans = -1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if (canMake(bloomDay, m, k, mid)) {
+                ans = mid;
+                high = mid - 1;   // try smaller day
+            } else {
+                low = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+};
