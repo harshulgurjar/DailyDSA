8271435fef482f015c4351df582e1bbb3e0e3b08
harshulgurjar <harshulgurjar7@gmail.com>
2026-07-09 20:44:09 +0530

    Time: 3 ms (72.48%), Space: 12.1 MB (70.1%) - LeetHub

---
+++ 0139-word-break/0139-word-break.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool solve(int index,string &s,unordered_set<string>&st,vector<int>&dp){
+        if(index == s.size()){
+            return true;
+        }
+        if(dp[index]!=-1)
+        return dp[index];
+
+        string temp="";
+
+        for(int i=index;i<s.size();i++){
+            temp+=s[i];
+
+            if(st.count(temp)){
+                if(solve(i+1,s,st,dp))
+                    return dp[index]=1;
+            }
+        }
+        return dp[index]=0;
+    }
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> st(wordDict.begin(),wordDict.end());
+        vector<int>dp(s.size(),-1);
+        return solve(0,s,st,dp);
+    }
+};
