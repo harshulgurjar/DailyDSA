85256e1638c9996d984c7a01b62de90e7d07f783
harshulgurjar <harshulgurjar7@gmail.com>
2026-07-23 15:57:40 +0530

    Time: 0 ms (100%), Space: 7.8 MB (93.86%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int dp[101][101]{};
+    int sol(int m,int n ,int i,int j){
+        if(i>=m || j>=n){
+            return 0;
+        }
+        if(i==m-1 && j==n-1){
+            return 1;
+        }
+        if(dp[i][j])
+            return dp[i][j];
+        return dp[i][j] = sol(m,n,i+1,j)+sol(m,n,i,j+1);
+    }
+    int uniquePaths(int m, int n) {
+        return sol(m,n,0,0);
+    }
+};
